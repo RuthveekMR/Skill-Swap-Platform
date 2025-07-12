8b1f6db965da7233adbf314d2dcc9135ee225a4d
Dhanwin007 <156595276+Dhanwin007@users.noreply.github.com>
2025-07-12 20:47:10 +0530

    Update api.js

---
+++ api.js
@@ -0,0 +1,11 @@
+async function fetchWithToken(url, options = {}) {
+  const token = localStorage.getItem("token");
+  return fetch(url, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token,
+      ...options.headers,
+    }
+  });
+}
